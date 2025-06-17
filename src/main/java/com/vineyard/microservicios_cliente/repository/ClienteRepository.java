c5d8ff9bdc0b8ba8f1dd0f5e7df4068617659f1e
IvanS <iv.santander@duocuc.cl>
2025-06-17 17:19:54 -0400

    Se eliminan piezas de codigo sin usar

--- src/main/java/com/vineyard/microservicios_cliente/repository/ClienteRepository.java
+++ src/main/java/com/vineyard/microservicios_cliente/repository/ClienteRepository.java
@@ -1,20 +1,10 @@
 package com.vineyard.microservicios_cliente.repository;
 
 import com.vineyard.microservicios_cliente.model.Cliente;
-
-import java.util.List;
-
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    
-    // Busca a todos los clientes
-    List<Cliente> findAll(Sort appaterno);
-
-    // Busca por ID
-    Cliente findById(Integer id);
 
 }
