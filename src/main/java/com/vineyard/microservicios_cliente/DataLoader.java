30e67930e135fae0bb0d2b52cee538e8040c6855
IvanS <iv.santander@duocuc.cl>
2025-06-17 18:22:09 -0400

    Se crea dataLoader pero con errores

---
+++ src/main/java/com/vineyard/microservicios_cliente/DataLoader.java
@@ -0,0 +1,37 @@
+package com.vineyard.microservicios_cliente;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.vineyard.microservicios_cliente.model.Cliente;
+import com.vineyard.microservicios_cliente.repository.ClienteRepository;
+
+import net.datafaker.Faker;
+@Component
+public class DataLoader implements CommandLineRunner{
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Override
+    public void run(String... args) throws Exception {
+        Faker faker = new Faker();
+
+        //Generar clientes
+        for (int i = 0; i < 10; i++) {
+            //Genera y rellena los datos
+            Cliente cliente = new Cliente();
+            cliente.setId(i+1);
+            cliente.setNombre(faker.name().firstName());
+            cliente.setAppaterno(faker.name().lastName());
+            cliente.setCorreo(faker.internet().emailAddress());
+            cliente.setTelefono(String.valueOf(faker.number().numberBetween(100000000,999999999)));
+            cliente.setDireccion(faker.address().streetAddress());
+
+            //Guarda el cliente
+            clienteRepository.save(cliente);
+        }
+        
+    }
+}
